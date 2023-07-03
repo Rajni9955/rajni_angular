d2bf9a10496c8ed1153585a53cad920af8552d40
rajniwamankar595@gmail.com <you@example.com>
2023-07-03 11:46:23 +0530

    component creation

--- ANGULAR_PROJECT/src/app/app.component.ts
+++ ANGULAR_PROJECT/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ANGULAR_PROJECT';
+  firstName : string ="Rajni";   //propertyName : datatype = value/data
+  firstName1 = "rajni";
+  firstName2! : string;
+  surName : any;
+  surName1 : any = true;
+  formTitle : string = "Login Form..."
+  studentName = 'angel';
+  amount = 5600;
+  myName = "rs";
+  isFavCity = false;
+  gender = false;
+  eventBindingData = 60000;
+  twoWayBindingTest = 26451;
+
+  testingInterpolation(x : number, y : number){
+    
+      return x+y;
+  }
+  testingEventBinding(){
+    this.eventBindingData = 50000;
+    console.log(this.eventBindingData);
+    
+  }
+
+  test(){
+    console.log(this.twoWayBindingTest);
+  }
 }
