a0490be0b40d43b7593cc57a829a9a529c94e3ce
Rajni9955 <rajniwamankar595@gmail.com>
2023-07-08 13:47:36 +0530

    component creation...

--- ANGEL_ANGULAR_PROJECT/src/app/app-routing.module.ts
+++ ANGEL_ANGULAR_PROJECT/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component : LoginComponent},
+  {path : 'signUp', component : SignUpComponent},
+  {path: '', component: HomeComponent},
+  {path : 'admin', loadChildren:()=>import('./admin/admin.module').then(mod=>mod.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ANGEL_ANGULAR_PROJECT/src/app/app.component.ts
+++ ANGEL_ANGULAR_PROJECT/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ANGEL_ANGULAR_PROJECT';
+// testcase1
+firstName : string = 'angel';
+// testcase2
+firstName1 = 'angggel';
+firstName2! : string;
+surName1 : any;
+surName : any = true;
+formTitle : string = 'Login Form...' ;
+studentName : string ='shiv';
+amount = 36545;
+isFavCity = false;
+myName="rani";
+gender=true;
+isDisabled=false;
+eventBindingData=55689;
+twowaybindingTest=5555;
+
+testingInterpolation(x: number, y: number){
+  return x + y;
 }
+
+testingeventbinding(){
+  this.eventBindingData = 6546721;
+  console.log(this.eventBindingData);
+  
+}
+
+test(){
+  console.log(this.twowaybindingTest);
+  
+}
+}
+
